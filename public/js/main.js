b1d0496c978b58cf782668c692f91bb4edd227a6
Jennilee Melgar <jennileemelgar@jennilees-mbp.myfiosgateway.com>
2023-12-07 10:34:16 -0500

    done done

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+const deleteBtn = document.querySelectorAll('.fa-trash')/*creating a viariable and assigning it to a section of all elements with a class of the trash can*/
+const item = document.querySelectorAll('.item span')/*creating a viariable and assigning it to a section span tags inside of a parent that has a class of "item"*/
+const itemCompleted = document.querySelectorAll('.item span.completed')/*creating a viariable and assigning it to a section of spans with a class of "completed" inside of a parent with a class of "item"*/
+
+Array.from(deleteBtn).forEach((element)=>{/*creating an array from our section and starting a loop*/
+    element.addEventListener('click', deleteItem)/*add an event listener to the current item that waits for a click and then calls a function called deleteitem*/
+})/*close our loop*/
+
+Array.from(item).forEach((element)=>{/*creating an array from our selection and starting a loop*/
+    element.addEventListener('click', markComplete)/*add an event listener to the current item that waits for a click and then calls a function called markcomplete*/
+})/*close our loop*/
+
+Array.from(itemCompleted).forEach((element)=>{/*creating an array from our selection and starting a loop*/
+    element.addEventListener('click', markUnComplete)/*adds an event listener to only completed items*/
+})/*close our loop*/
+
+async function deleteItem(){/*declare an asynchronous function*/
+    const itemText = this.parentNode.childNodes[1].innerText/*looks inside of the list item and grabs only the inner text within the list span.*/
+    try{/*starting a try block to do something*/
+        const response = await fetch('deleteItem', {/*creates a response variable that waits on a fetch to get data from te results of the deleteItem route*/
+            method: 'delete',/*sets the CRUD method for the route*/
+            headers: {'Content-Type': 'application/json'},/*specifying the type of content expected, which is JSON*/
+            body: JSON.stringify({/*declare the massage content being passed, and stringly that content*/
+              'itemFromJS': itemText/*setting the content of the body to the inner text to the item and naming it 'itemFromJS'*/
+            })/*closing the body*/
+          })/*closing the object*/
+        const data = await response.json()/*waiting on JSON  from the response to be converted*/
+        console.log(data)/*log the result to the console*/
+        location.reload()/*reloads the page to update what is displayed*/
+
+    }catch(err){/*if an error occurs, pass the error into the catch block*/
+        console.log(err)/*log the error to the console*/
+    }/*close the catch block*/
+}/*end the function*/
+
+async function markComplete(){/*declare an asynchronous function*/
+    const itemText = this.parentNode.childNodes[1].innerText/*looks inside of the list item and grabes only the inner text within the list span*/
+    try{/*starting a try block to do something*/
+        const response = await fetch('markComplete', {/*creates a response variable that waits on a fetch to get data from the result of the markComplete route*/
+            method: 'put',/*setting  the CRUD method to "update" for the route*/
+            headers: {'Content-Type': 'application/json'},/*specifying the type of content expected, which is JSON*/
+            body: JSON.stringify({/*declare the message content being passed, and stringify that content*/
+                'itemFromJS': itemText/*setting the content of the body to the inner text of the list item, and naming it 'itemFromJS'*/
+            })/*closing the body*/
+          })/*closing the object*/
+        const data = await response.json()/*waiting on JSON from the response to be converted*/
+        console.log(data)/*log the result to the console*/
+        location.reload()/*reloads the page to update what is displayed*/
+
+    }catch(err){/*if an error occurs, pass the error into the catch block*/
+        console.log(err)/*log the error to the console*/
+    }//close the catch block
+}/*end the function*/
+
+async function markUnComplete(){/*declare an asynchronous function*/
+    const itemText = this.parentNode.childNodes[1].innerText/*looks inside of the list item and grabs only the inner text within the list span*/
+    try{/*starting a try block to do something*/
+        const response = await fetch('markUnComplete', {/*creates a response variable that waits on a fetch to get data from the result of the markUncomplete route*/
+            method: 'put',/*setting the CRUD method to "update" for the route*/
+            headers: {'Content-Type': 'application/json'},/*specifying the type of content expected, which is JSON*/
+            body: JSON.stringify({/*declare the message content being passed, and stringify that content*/
+                'itemFromJS': itemText/*setting of the body to the inner text of the list item and naming it*/
+            })/*closing the body*/
+          })/*closing the object*/
+        const data = await response.json()/**/
+        console.log(data)/**/
+        location.reload()/**/
+
+    }catch(err){/**/
+        console.log(err)/**/
+    }/**/
+}/**/
